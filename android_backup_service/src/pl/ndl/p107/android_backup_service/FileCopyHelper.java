e58b15c6b6d30caafe814a09f67569d3df3550a3
p107 <p107@local>
2015-01-16 13:58:43 +0100

    2 funkcje do przetestowania

---
+++ android_backup_service/src/pl/ndl/p107/android_backup_service/FileCopyHelper.java
@@ -0,0 +1,61 @@
+package pl.ndl.p107.android_backup_service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileCopyHelper {
+
+	// TODO: test
+	public void copy(File src, File dst) throws IOException {
+		InputStream in = new FileInputStream(src);
+		OutputStream out = new FileOutputStream(dst);
+
+		// Transfer bytes from in to out
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
+	}
+
+	// TODO: test
+	public void copyDirectory(File sourceLocation, File targetLocation) throws IOException {
+		if (sourceLocation.isDirectory()) {
+			if (!targetLocation.exists() && !targetLocation.mkdirs()) {
+				throw new IOException("Cannot create dir " + targetLocation.getAbsolutePath());
+			}
+
+			String[] children = sourceLocation.list();
+			for (int i = 0; i < children.length; i++) {
+				copyDirectory(new File(sourceLocation, children[i]), new File(targetLocation, children[i]));
+			}
+		}
+		else {
+
+			// make sure the directory we plan to store the recording in exists
+			File directory = targetLocation.getParentFile();
+			if (directory != null && !directory.exists() && !directory.mkdirs()) {
+				throw new IOException("Cannot create dir " + directory.getAbsolutePath());
+			}
+
+			InputStream in = new FileInputStream(sourceLocation);
+			OutputStream out = new FileOutputStream(targetLocation);
+
+			// Copy the bits from instream to outstream
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			in.close();
+			out.close();
+		}
+	}
+
+}
